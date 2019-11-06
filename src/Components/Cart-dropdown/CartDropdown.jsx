fd2b55df9e544dca89c72391999fd18a6e76e42f
Dextrous <ssjdexucyche@gmail.com>
2019-11-06 17:31:51 +0530

    finished cart dropdown UI

--- src/Components/Cart-dropdown/CartDropdown.jsx
+++ src/Components/Cart-dropdown/CartDropdown.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./CartDropdown.scss";
 import Button from "../Buttons/Button";
-export default function CartDropdown() {
+import { connect } from "react-redux";
+import CartItem from "../Cart-Item/CartItem";
+function CartDropdown({ cartItems }) {
   return (
     <div class="cart-dropdown">
       <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
         <Button value="GO TO CHECKOUT"></Button>
       </div>
     </div>
   );
 }
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+export default connect(mapStateToProps)(CartDropdown);
