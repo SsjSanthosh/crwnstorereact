9b43a50570015eb5e25338b6ed2e9fd90801194f
Dextrous <ssjdexucyche@gmail.com>
2019-11-07 16:53:36 +0530

    Updates

---
+++ precache-manifest.c38550df997450a321b364722634d95c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427b84493885eec350c7a7b56e380160",
+    "url": "/crwnstorereact/index.html"
+  },
+  {
+    "revision": "a3b800347e07581c633a",
+    "url": "/crwnstorereact/static/css/main.ae7fe1f2.chunk.css"
+  },
+  {
+    "revision": "4c7e226507a8b14ce3ae",
+    "url": "/crwnstorereact/static/js/2.09b6af4a.chunk.js"
+  },
+  {
+    "revision": "a3b800347e07581c633a",
+    "url": "/crwnstorereact/static/js/main.55f040a4.chunk.js"
+  },
+  {
+    "revision": "981dafda160caeeb9081",
+    "url": "/crwnstorereact/static/js/runtime-main.a2bb5a08.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwnstorereact/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwnstorereact/static/media/shopping-bag.30f07071.svg"
+  }
+]);
